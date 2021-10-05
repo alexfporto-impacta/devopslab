296478e6f4614888373c893df9111c25c51eed0f
alexfporto-impacta <91922461+alexfporto-impacta@users.noreply.github.com>
2021-10-04 23:25:54 -0300

    Com o link para correção do erro SSL

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Hello World
+    Comando para correção do erro de SSL https://docs.travis-ci.com/user/reference/focal/"
 
 if __name__ == '__main__':
     app.run(debug=True)
