3021c884b48c83720cb608d87057af7c198199b3
alexfporto-impacta <91922461+alexfporto-impacta@users.noreply.github.com>
2021-10-04 23:30:27 -0300

    Corrigido Erro SSL

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World!! Comando para correção do erro de SSL https://docs.travis-ci.com/user/reference/focal/"
+    return "Hello World!! Corrigido ERRO SSL"
 
 if __name__ == '__main__':
     app.run(debug=True)
